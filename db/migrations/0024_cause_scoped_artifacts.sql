e5a4e05311f1e9ffdfdb1cff350bd286b7d0212d
Clickabl <tech@clickabl.co>
2026-09-20 17:33:27 -0600

    fix: make artifact cause migration valid on a clean database

--- db/migrations/0024_cause_scoped_artifacts.sql
+++ db/migrations/0024_cause_scoped_artifacts.sql
@@ -4,33 +4,24 @@ ALTER TABLE publication_artifacts
   ADD COLUMN cause_slug text NOT NULL DEFAULT 'israel-palestine'
   REFERENCES causes(slug);
 
-DO $
-DECLARE
-  constraint_name text;
+DO $$
+DECLARE constraint_name text;
 BEGIN
-  SELECT conname
-  INTO constraint_name
-  FROM pg_constraint
-  WHERE conrelid = 'publication_artifacts'::regclass
-    AND contype = 'u'
-  LIMIT 1;
-
-  IF constraint_name IS NOT NULL THEN
+  FOR constraint_name IN
+    SELECT conname FROM pg_constraint
+    WHERE conrelid = 'publication_artifacts'::regclass AND contype = 'u'
+  LOOP
     EXECUTE format('ALTER TABLE publication_artifacts DROP CONSTRAINT %I', constraint_name);
-  END IF;
-END
-$;
+  END LOOP;
+END;
+$$;
 
-DROP INDEX publication_artifacts_full_uq;
-DROP INDEX publication_artifacts_delta_uq;
+DROP INDEX IF EXISTS publication_artifacts_identity_uq;
+DROP INDEX IF EXISTS publication_artifacts_full_uq;
+DROP INDEX IF EXISTS publication_artifacts_delta_uq;
 
-CREATE UNIQUE INDEX publication_artifacts_full_uq
-  ON publication_artifacts(publication_id, cause_slug, channel, list_kind, version)
-  WHERE artifact_kind = 'full';
-
-CREATE UNIQUE INDEX publication_artifacts_delta_uq
-  ON publication_artifacts(publication_id, cause_slug, channel, list_kind, version, base_version)
-  WHERE artifact_kind = 'delta';
+CREATE UNIQUE INDEX publication_artifacts_identity_uq
+  ON publication_artifacts(publication_id, cause_slug, channel, list_kind, artifact_kind, version, COALESCE(base_version, ''));
 
 CREATE INDEX publication_artifacts_cause_lookup_idx
   ON publication_artifacts(cause_slug, channel, list_kind, artifact_kind, version);
