64670187d54c1ce68ac78f94371df8a874c88035
Clickabl <tech@clickabl.co>
2026-09-20 17:30:46 -0600

    db: remove generated artifact uniqueness constraint robustly

--- db/migrations/0024_cause_scoped_artifacts.sql
+++ db/migrations/0024_cause_scoped_artifacts.sql
@@ -4,8 +4,22 @@ ALTER TABLE publication_artifacts
   ADD COLUMN cause_slug text NOT NULL DEFAULT 'israel-palestine'
   REFERENCES causes(slug);
 
-ALTER TABLE publication_artifacts
-  DROP CONSTRAINT publication_artifacts_publication_id_channel_list_kind_art_key;
+DO $
+DECLARE
+  constraint_name text;
+BEGIN
+  SELECT conname
+  INTO constraint_name
+  FROM pg_constraint
+  WHERE conrelid = 'publication_artifacts'::regclass
+    AND contype = 'u'
+  LIMIT 1;
+
+  IF constraint_name IS NOT NULL THEN
+    EXECUTE format('ALTER TABLE publication_artifacts DROP CONSTRAINT %I', constraint_name);
+  END IF;
+END
+$;
 
 DROP INDEX publication_artifacts_full_uq;
 DROP INDEX publication_artifacts_delta_uq;
