4429513f99b7990ef52e2866e0d38797c90109b2
Clickabl <tech@clickabl.co>
2026-09-20 17:29:37 -0600

    db: replace actual publication artifact uniqueness constraints

--- db/migrations/0024_cause_scoped_artifacts.sql
+++ db/migrations/0024_cause_scoped_artifacts.sql
@@ -4,17 +4,19 @@ ALTER TABLE publication_artifacts
   ADD COLUMN cause_slug text NOT NULL DEFAULT 'israel-palestine'
   REFERENCES causes(slug);
 
-DROP INDEX publication_artifacts_identity_uq;
+ALTER TABLE publication_artifacts
+  DROP CONSTRAINT publication_artifacts_publication_id_channel_list_kind_art_key;
 
-CREATE UNIQUE INDEX publication_artifacts_identity_uq
-  ON publication_artifacts(
-    publication_id,
-    cause_slug,
-    channel,
-    list_kind,
-    artifact_kind,
-    COALESCE(base_version, '')
-  );
+DROP INDEX publication_artifacts_full_uq;
+DROP INDEX publication_artifacts_delta_uq;
+
+CREATE UNIQUE INDEX publication_artifacts_full_uq
+  ON publication_artifacts(publication_id, cause_slug, channel, list_kind, version)
+  WHERE artifact_kind = 'full';
+
+CREATE UNIQUE INDEX publication_artifacts_delta_uq
+  ON publication_artifacts(publication_id, cause_slug, channel, list_kind, version, base_version)
+  WHERE artifact_kind = 'delta';
 
 CREATE INDEX publication_artifacts_cause_lookup_idx
   ON publication_artifacts(cause_slug, channel, list_kind, artifact_kind, version);
